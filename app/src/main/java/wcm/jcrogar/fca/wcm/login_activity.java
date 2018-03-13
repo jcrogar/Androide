d287b64cc84ee3aa7a86126eea4b9cc77911740b
JCROGAR <jorgegr7774@gmail.com>
2018-03-12 21:42:09 -0600

    kk

--- app/src/main/java/wcm/jcrogar/fca/wcm/login_activity.java
+++ app/src/main/java/wcm/jcrogar/fca/wcm/login_activity.java
@@ -1,5 +1,7 @@
 package wcm.jcrogar.fca.wcm;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,15 @@ public class login_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent splash = new Intent(login_activity.this, MainActivity.class);
+                startActivity(splash);
+
+            }
+        },40000);
     }
 }
