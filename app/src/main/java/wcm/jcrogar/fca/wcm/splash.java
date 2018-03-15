43b8901c773fa6697b36af2a0d19cc538f262716
jcrogar <jorgegr7774@gmail.com>
2018-03-15 14:56:35 -0600

    splash background anim Commit 0256 1503

--- app/src/main/java/wcm/jcrogar/fca/wcm/splash.java
+++ app/src/main/java/wcm/jcrogar/fca/wcm/splash.java
@@ -1,17 +1,20 @@
 package wcm.jcrogar.fca.wcm;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ProgressBar;
 
 public class splash extends AppCompatActivity {
-    private final int DURACION_SPLASH = 3000;
-
+    private final int DURACION_SPLASH = 10000;
+    ConstraintLayout myLayout;
+    AnimationDrawable animationDrawable;
     public ProgressBar splash_progressbar;
-    public int MAX_VALUE = 30;
+    public int MAX_VALUE = 100;
 
 
 
@@ -20,8 +23,15 @@ public class splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+        myLayout = (ConstraintLayout) findViewById(R.id.myLayout);
+        animationDrawable = (AnimationDrawable) myLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(3000);
+        animationDrawable.setExitFadeDuration(3000);
+        animationDrawable.start();
+
+
         splash_progressbar = (ProgressBar) findViewById(R.id.progressBar);
-        splash_progressbar.setMax(30);
+        splash_progressbar.setMax(100);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -32,9 +42,9 @@ public class splash extends AppCompatActivity {
             }
         }, DURACION_SPLASH);
 
-        new CountDownTimer(3000, 100) {
+        new CountDownTimer(10000, 100) {
 
-            int progreso = 1; // Variable que va a ir aumentando del progreso
+            int progreso = 1;
             @Override
             public void onTick(long millisUntilFinished) {
                 splash_progressbar.setProgress(progreso);
