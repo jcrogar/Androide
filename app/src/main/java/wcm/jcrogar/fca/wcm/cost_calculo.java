96317aee14aa9da569c916d07c337898aff686ce
jcrogar <jorgegr7774@gmail.com>
2018-04-10 11:00:59 -0500

    CALC Commit 1100 1004

--- app/src/main/java/wcm/jcrogar/fca/wcm/cost_calculo.java
+++ app/src/main/java/wcm/jcrogar/fca/wcm/cost_calculo.java
@@ -8,12 +8,22 @@ import android.widget.Button;
 
 public class cost_calculo extends AppCompatActivity {
     Button btnQuick;
+    Button btnStandar;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cost_calculo);
         btnQuick = (Button) findViewById(R.id.btnQuick);
+        btnStandar = (Button) findViewById(R.id.btnStandard);
+
+        btnStandar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Graf = new Intent(cost_calculo.this, pruebaGrafica.class);
+                startActivity(Graf);
+            }
+        });
 
         btnQuick.setOnClickListener(new View.OnClickListener() {
             @Override
